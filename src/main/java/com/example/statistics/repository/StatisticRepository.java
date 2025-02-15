8190e45550177349bb0d7785af6c292b26d41f07
Solopenko2005 <solopenko2005@gmail.com>
2025-02-15 14:47:00 +0300

    Delete src directory

--- src/main/java/com/example/statistics/repository/StatisticRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.statistics.repository;
-
-import com.example.statistics.model.StatisticEvent;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StatisticRepository extends MongoRepository<StatisticEvent, String> {
-}
